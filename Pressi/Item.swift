ca67672231742ab72739d2c4f0d1464c70e2d8cc
Dino Omanovic <dino.omanovic@icloud.com>
2025-09-03 23:00:21 +0200

    Initial Commit

---
+++ Pressi/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Pressi
+//
+//  Created by Dino Omanovic on 2025-09-03.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PressiTests/PressiTests.swift
@@ -0,0 +1,16 @@
+//
+//  PressiTests.swift
+//  PressiTests
+//
+//  Created by Dino Omanovic on 2025-09-03.
+//
+
+import Testing
+
+struct PressiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
